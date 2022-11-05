5244175caf6d0881f494e0a3c812605946af2406
Bhuvaneshwaran I <bhpo00001@stud.uni-saarland.de>
2022-11-05 12:55:04 +0100

    Added MyPath.java

---
+++ src/main/java/rddl/competition/MyPath.java
@@ -0,0 +1,18 @@
+package rddl.competition;
+
+public class MyPath {
+    public String getProjectPath() {
+        java.net.URL url = MyPath.class.getProtectionDomain().getCodeSource().getLocation();
+        String filePath = null;
+        try {
+            filePath = java.net.URLDecoder.decode(url.getPath(), "utf-8");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if (filePath.endsWith(".jar"))
+            filePath = filePath.substring(0, filePath.lastIndexOf("/") + 1);
+        java.io.File file = new java.io.File(filePath);
+        filePath = file.getAbsolutePath();
+        return filePath;
+    }
+}
--- src/main/java/rddl/competition/Records.java
+++ src/main/java/rddl/competition/Records.java
@@ -5,23 +5,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 
-class MyPath {
-    public String getProjectPath() {
-       java.net.URL url = MyPath.class .getProtectionDomain().getCodeSource().getLocation();
-       String filePath = null ;
-       try {
-           filePath = java.net.URLDecoder.decode (url.getPath(), "utf-8");
-       } catch (Exception e) {
-           e.printStackTrace();
-       }
-    if (filePath.endsWith(".jar"))
-       filePath = filePath.substring(0, filePath.lastIndexOf("/") + 1);
-    java.io.File file = new java.io.File(filePath);
-    filePath = file.getAbsolutePath();
-    return filePath;
-    }
-}
-    
 public class Records {
 	public boolean fileAppend(String fileName, String context)
 	{
